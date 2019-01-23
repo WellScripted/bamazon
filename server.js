a1153f18878e95c0213e08fccf495089e0470b03
Scripted Well <whigareda@theblackpilledpage.com>
2019-01-23 07:24:11 -0700

    Added psuedocode & selectQuantity function

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
     database: "bamazon"
 })
 
-connection.connect(function (err){
+connection.connect(function (err) {
     if (err) throw err;
     console.log("SUCCESS!");
     makeTable();
@@ -20,10 +20,10 @@ connection.connect(function (err){
 
 //Make Table
 var makeTable = function () {
-    connection.query("SELECT * FROM products", function(err, res){ 
+    connection.query("SELECT * FROM products", function (err, res) {
         console.table(res);
         selectItem(res);
-    }) 
+    })
 }
 
 //Select Item
@@ -35,7 +35,7 @@ var selectItem = function (inventory) {
         validate: function (value) {
             return !isNaN(value) || value.toLowerCase() === "q";
         }
-    }]).then(function (value){
+    }]).then(function (value) {
         userExit(value.userChoice);
         var choiceID = parseInt(value.userChoice);
         var product = checkInventory(choiceID, inventory);
@@ -50,20 +50,49 @@ var selectItem = function (inventory) {
 
 //Select Quantity
 var selectQuantity = function () {
-    //fill with code later
+    inquirer.prompt({
+        type: "input",
+        name: "UserChoice",
+        message: "How many would you like? [Quit with Q]",
+        validate: function (value) {
+            return !isNaN(value) || value.toLowerCase() === "q";
+        }
+    }).then(function (value) {
+        userExit(value.userChoice);
+        var choiceID = parseInt(value.userChoice);
+        var quantity = checkInventory(choiceID, inventory);
+        if (quantity > inventory) {
+            console.log("The demand exceeds the supply. Don't be greedy, select fewer items");
+        } else {
+            purchaseItem();
+            makeTable();
+        }
+        
+    })
+    // Inquirer prompt for quantity wanted
+    // Make sure to give quit option and validate (just like above)
+    // Check if should exit by calling the exit function
+    // if quantity desired is greater than stock quantity, print a message alerting the user.
+    // else call the purchaseItem function
 }
 
 //Purchase Item
 var purchaseItem = function () {
-    //fill with code later
+    // Connect to mySQL and update the stock_quantity of the item purchased
+    // Console log a message the purchase was successful
+    // Call makeTable function
 }
 
 //Check Inventory
 var checkInventory = function () {
-    //fill with code later
+    // Loop through all inventory
+    // if choiceID === item_id, return that inventory item
+    // otherwise return null
 }
 
 //userExit
 var userExit = function () {
-    //fill with code later
+    // If user enters "q" at any point, 
+    // console log a goodbye statement to the user
+    // Exit the process (using process.exit(0))
 }
